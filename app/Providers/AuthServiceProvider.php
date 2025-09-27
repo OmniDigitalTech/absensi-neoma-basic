48932f8c4ec3818f60de2b89ddb37b97786d8fbe
Ancasea <rekasae255@gmail.com>
2025-09-27 18:47:33 +0700

    Upgrade laravel framework 9 to 10, and other required package like in the laravel documentation also third party package

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -23,7 +23,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
+//        $this->registerPolicies();
 
         Gate::define('viewWebTinker', function ($user = null) {
             // return true if access to web tinker is allowed
